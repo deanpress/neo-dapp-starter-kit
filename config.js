9b5e0700eb014ce424c76c7026a24f0bb81f121d
Dean van Dugteren <31391056+deanpress@users.noreply.github.com>
2017-11-26 00:56:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,19 @@
 module.exports = {
-    // scriptHash is your smart contract Script Hash, without the first 2 '0x' characters
-    // To retrieve your Script Hash: (in neo-gui: right click smart contract address > View Contract)
-    scriptHash: '902c3c871ff596e69cf69fd4e289fa5b884f1aca',
-    // RESTEndpoint is an endpoint to a neon-wallet-db REST server that's connected to your desired net (MainNet, TestNet, PrivNet)
-    // MainNet: http://api.wallet.cityofzion.io
-    // TestNet: http://testnet-api.wallet.cityofzion.io
-    // PrivNet: Your neon-wallet-db instance IP with port 5000
-    RESTEndpoint: 'http://127.0.0.1:5000',
-    wif: 'KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr',
+    
+    /* 
+     * scriptHash is your smart contract Script Hash, without the first 2 '0x' characters
+     * To retrieve your Script Hash: (in neo-gui: right click smart contract address > View Contract)
+     */
+     scriptHash: '902c3c871ff596e69cf69fd4e289fa5b884f1aca',
+    
+    /*
+     * RESTEndpoint is an endpoint to a neon-wallet-db REST server that's connected to your desired net (MainNet, TestNet, PrivNet)
+     * MainNet: http://api.wallet.cityofzion.io
+     * TestNet: http://testnet-api.wallet.cityofzion.io
+     * PrivNet: Your neon-wallet-db instance IP with port 5000
+     */
+     RESTEndpoint: 'http://127.0.0.1:5000',
+    
+     wif: 'KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr',
+    
 };
