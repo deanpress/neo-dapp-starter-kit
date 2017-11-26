d03a192d685ede09203f5927a5d7ab24a7e4963f
Dean van Dugteren <31391056+deanpress@users.noreply.github.com>
2017-11-26 17:17:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const Neon = require('@cityofzion/neon-js').default
 const node = require('./blockchain')
 const config = require('./config')
 const account = Neon.create.account(config.wif)
 
-// node.getStorage('AGs7dP8tX1KgjXopLELdmZed85GnEkFPRh').then((res) => {
-node.getStorage('TomciuIsGekk').then((res) => {
-    if (typeof res === "string") {
-        console.log(res);
+// Invoke a smart contract with a method and an array of strings
+node.invokeContract('WriteToStorage', ['someStorageKey', 'Hello World!'], account, (res) => {
+    if (res.result === true) {
+        // Transaction successful. The stored data can be retrieved on the next block.
+        console.log('Transaction processed!')
+    } else {
+        console.log('Transaction has not been processed.')
     }
+})
+
+// Get a storage key
+node.getStorage('someStorageKey').then((res) => {
+    if (typeof res === "string") {
+        //Do something with the resulting value
+    }
+})
+
+// Test a transaction. This returns expected values from your smart contract, and doesn't send a transaction. Costs 0 gas.
+// Note: These are not signed transactions, so it doesn't return data that uses a sender's address.
+node.testContract('WriteToStorage', ['someStorageKey', 'Hello World!'], (res) => {
+    console.dir(res)
 })
