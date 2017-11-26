60df6320e7196f0c217cda188981bae8c2765570
Dean <hello@dean.press>
2017-11-26 17:20:03 +0100

    Include neon-js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const neon = require('@cityofzion/neon-js');
+const Neon = neon.default;
 const node = require('./blockchain')
 const config = require('./config')
 const account = Neon.create.account(config.wif)
